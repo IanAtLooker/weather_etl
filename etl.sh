545ee5446d5307152d254a09dad7a3693d6b01c3
Carter Moar <cartermoar@Carters-MacBook-Air.local>
2015-11-30 22:11:53 -0800

    commit because who can remember the scp syntax

--- etl.sh
+++ etl.sh
@@ -20,11 +20,14 @@ echo "station_insert.txt removed on `date -u`"
 python weather.py
 # eventually this should just call a function and pass the file in
 
+# For now, just truncate the table and re-populate
+PGHOST=$PGHOST PGUSER=$PGUSER PGPORT=$PGPORT PGPASSWORD=$PGPASSWORD \
+	/usr/bin/psql -c "TRUNCATE TABLE stations;"
+echo "stations truncated on `date -u`"
 
 while read line; do 
 	PGHOST=$PGHOST PGUSER=$PGUSER PGPORT=$PGPORT PGPASSWORD=$PGPASSWORD \
 	/usr/bin/psql -c "$line;"
-    echo $line
 done < station_insert.txt
 
 echo "Sleeping on `date -u`"
@@ -32,5 +35,3 @@ echo "Sleeping on `date -u`"
 sleep 43200 
 
 done
-
-# psql -U wunderground -h fwd-us.cn1001q2lzdp.us-east-1.rds.amazonaws.com
