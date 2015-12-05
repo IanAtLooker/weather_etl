5065f0dc044772a4bfc2cb47d6c8c6bfea318d74
Carter Moar <cartermoar@Carters-MacBook-Air.local>
2015-12-04 20:39:14 -0800

    moving some stuff around and also scping via git

--- weather.py
+++ weather.py
@@ -13,13 +13,17 @@ weatherKey1 = readers_and_writers.fileReader('.wukey1')
 pgHostName = readers_and_writers.fileReader('.weatherhost')
 pgWord = readers_and_writers.fileReader('.pgwrd')
 
-# Still doing this stuff in python instead of shell so we can do fancier stuff later
 
 
-###		Get station data
+###		Write station data
 
-scStations = getters_and_setters.updateStations(weatherKey1, 'ca', 'santa_cruz')
-readers_and_writers.fileWriter('station_insert.txt', scStations)
+def stationFileWriter(api_key, state, city):
+	stations = getters_and_setters.updateStations(api_key, state, city)
+	readers_and_writers.fileWriter('station_insert.txt', stations)
+	
+
+# scStations = getters_and_setters.updateStations(weatherKey1, 'ca', 'santa_cruz')
+# readers_and_writers.fileWriter('station_insert.txt', scStations)
 
 
 ###		Make it still do what it orginally did for now
